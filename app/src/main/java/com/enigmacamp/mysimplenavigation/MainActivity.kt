b8b541c00f8feaf5139714152e9a9f75def5e7eb
edwardsuwirya <edward_suwirya@yahoo.com>
2023-03-09 20:53:23 +0700

    Simple jetpack navigation

---
+++ app/src/main/java/com/enigmacamp/mysimplenavigation/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.enigmacamp.mysimplenavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+/*
+1. Install jetpack navigation di build.gradle (app)
+2.
+
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
